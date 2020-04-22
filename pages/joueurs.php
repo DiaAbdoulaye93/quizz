ca1c49e5dbb78226c4d9eebf2e696b3303163056
Abdoulaye Dia <diaabdoulayedjibril@gmail.com>
2020-04-22 00:40:36 +0000

    Ajout de la parination de la liste des joeurs

--- pages/joueurs.php
+++ pages/joueurs.php
@@ -1,5 +1,6 @@
 <?php   
  $List_users=getData();
+
 ?>
 <div class="listusers" id="listusers">
     <div class="titre">LISTE DES JOUEURS PAR SCORE</div>
@@ -16,23 +17,39 @@
          <tbody>  
 
         <?php   
+
+if(isset($_GET['page']))
+{
+       $page= $_GET['page'];
+}
+else{
+ $page= 1;
+ }
+        $nbrparPage=15;
+        $total=count($List_users);
+        $nombredepages= ceil($total/$nbrparPage);
+        $_SESSION['users'] =$List_users;
+       $min=($page-1)*$nbrparPage;
+        $max=$min+$nbrparPage-1;
         
-        
-          rsort($List_users );
-        for ($i=0; $i <count($List_users); $i++): 
+          rsort($List_users);
+       
+        for ($i=$min; $i<=$max; $i++): 
         if ("joueur"==$List_users[$i]['profil']) {
-            ?>
+           
+           ?>
             <tr> 
             <td><?php echo $List_users[$i]['nom']?> </td>
             <td><?php  echo $List_users[$i]['prenom']?></td>
             <td><?php  echo $List_users[$i]['score']?> pts</td>
         </tr>
-       
-
-           
+      
          <?php }  endfor; ?>
          </tbody>
      </table>
-      
+     <div class="paginate-liste-zone">
+        <button type="submit" name="suivant" class="suivant" id="suivant"><a href="index.php?lien=accueil&lien1=joueurs&page=<?= $page+1?>">SUIVANT</a></button>
+        </div>
+    
     </div>
 </div>
